9f3a9af0aa88d17389468aeb7b93e27d8561ab48
Silviu Simen <silviu.simen@gmail.com>
2024-03-19 14:03:15 +0000

    map refactoring (wip)

--- slideshowmaker/map/map_osm.py
+++ slideshowmaker/map/map_osm.py
@@ -1,28 +1,20 @@
 from ..models.cache import Cache
 from ..models.geolocation import Geolocation
 from .tile_manager import TileManager
-import geotiler
+
 from PIL import Image
 from .map import Map, MAP_DEFAULT_ZOOM, MAP_DEFAULT_SIZE_X, MAP_DEFAULT_SIZE_Y
 
 import math
 
-# import urllib3
-import requests
-import base64
-from io import BytesIO
-
-# import urllib2
-# import StringIO
-
-
 # https://stackoverflow.com/questions/28476117/easy-openstreetmap-tile-displaying-for-python
 # https://wiki.openstreetmap.org/wiki/Slippy_map_tilenames
 # https://wiki.openstreetmap.org/wiki/Raster_tile_providers
+# https://wiki.openstreetmap.org/wiki/Zoom_levels
 # https://{a|b|c}.tile.opentopomap.org/{z}/{x}/{y}.png
 
 
-def deg2num(lat_deg, lon_deg, zoom):
+def deg2num(lat_deg: float, lon_deg: float, zoom: int):
     lat_rad = math.radians(lat_deg)
     n = 1 << zoom
     xtile = int((lon_deg + 180.0) / 360.0 * n)
@@ -30,12 +22,18 @@ def deg2num(lat_deg, lon_deg, zoom):
     return xtile, ytile
 
 
-def num2deg(xtile, ytile, zoom):
+def num2deg(xtile: int, ytile: int, zoom: int):
     n = 1 << zoom
     lon_deg = xtile / n * 360.0 - 180.0
     lat_rad = math.atan(math.sinh(math.pi * (1 - 2 * ytile / n)))
     lat_deg = math.degrees(lat_rad)
     return lat_deg, lon_deg
+
+
+def zoom_to_tile_degree(zoom: int) -> float:
+    n = 1 << zoom
+    width_deg = 360.0 / n
+    return width_deg
 
 
 class Map_OSM(Map):
@@ -45,17 +43,14 @@ class Map_OSM(Map):
     def get_url(geo: Geolocation, zoom: int = 19) -> str:
         return f"https://www.openstreetmap.org/#map={zoom}/{geo.lat}/{geo.lon}"
 
-    def getImageCluster(self, lat_deg, lon_deg, delta_lat, delta_long, zoom):
-        xmin, ymax = deg2num(lat_deg, lon_deg, zoom)
-        xmax, ymin = deg2num(lat_deg + delta_lat, lon_deg + delta_long, zoom)
+    def getTileRanges(self, geo: Geolocation, zoom: int):
+        delta = zoom_to_tile_degree(zoom)
+        xmin, ymin = deg2num(geo.lat - delta / 2, geo.lon - delta / 2, zoom)
+        xmax, ymax = deg2num(geo.lat + delta / 2, geo.lon + delta / 2, zoom)
+        return (xmin, ymin, xmax, ymax)
 
-        bbox_ul = num2deg(xmin, ymin, zoom)
-        bbox_ll = num2deg(xmin, ymax + 1, zoom)
-        # print bbox_ul, bbox_ll
-
-        bbox_ur = num2deg(xmax + 1, ymin, zoom)
-        bbox_lr = num2deg(xmax + 1, ymax + 1, zoom)
-        # print bbox_ur, bbox_lr
+    def getImageCluster(self, geo: Geolocation, zoom: int):
+        xmin, ymin, xmax, ymax = self.getTileRanges(geo, zoom)
 
         Cluster = Image.new(
             "RGB", ((xmax - xmin + 1) * 256 - 1, (ymax - ymin + 1) * 256 - 1)
@@ -72,7 +67,7 @@ class Map_OSM(Map):
                     print(e)
                     tile = None
 
-        return Cluster, [bbox_ll[1], bbox_ll[0], bbox_ur[1], bbox_ur[0]]
+        return Cluster
 
     def render_map_image(
         self,
@@ -81,15 +76,5 @@ class Map_OSM(Map):
         sizex: int = MAP_DEFAULT_SIZE_X,
         sizey: int = MAP_DEFAULT_SIZE_Y,
     ) -> Image:
-        # map = geotiler.Map(
-        #     center=(geo.lon, geo.lat), zoom=zoom, size=(sizex, sizey), provider="osm"
-        # )
-        lat_deg, lon_deg, delta_lat, delta_long, zoom = (
-            45.720 - 0.04 / 2,
-            4.210 - 0.08 / 2,
-            0.04,
-            0.08,
-            14,
-        )
-        a, bbox = self.getImageCluster(lat_deg, lon_deg, delta_lat, delta_long, zoom)
+        a = self.getImageCluster(geo, zoom)
         return a
